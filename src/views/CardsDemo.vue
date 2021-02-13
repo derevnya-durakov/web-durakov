352bf0cb1d413cad09ce160f249a12d6ef4237c8
Sergey Sokolov <mohterbaord@gmail.com>
2021-02-14 00:27:42 +0300

    added flip animation when changing card

--- src/views/CardsDemo.vue
+++ src/views/CardsDemo.vue
@@ -4,7 +4,7 @@
       <button
         v-for="suit in suits"
         :key="suit"
-        @click="cardSuit = suit"
+        @click="setSuit(suit)"
       >
         {{suitSymbol(suit)}}
       </button>
@@ -13,24 +13,24 @@
       <button
         v-for="rank in ranks"
         :key="rank"
-        @click="cardRank = rank"
+        @click="setRank(rank)"
       >
         {{rankTitle(rank)}}
       </button>
     </div>
-    <flip style="margin: 0 auto;" :flipped="cardFlipped">
+    <flip style="margin: 0 auto;" :flipped="cardFlippedToFront">
       <template v-slot:front>
-        <card :model="cardModel" @click="flipBack"/>
+        <card :model="frontCardModel" @click="flipBack"/>
       </template>
       <template v-slot:back>
-        <card/>
+        <card :model="backCardModel" @click="flipBack"/>
       </template>
     </flip>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, Ref, ref } from 'vue';
 
 import { Suit, suits, suitSymbol, Rank, ranks, rankTitle } from '@/enums';
 import CardModel from '@/models/Card';
@@ -48,24 +48,85 @@ export default defineComponent({
   },
 
   setup() {
-    const cardSuit = ref<Suit | null>(null);
-    const cardRank = ref<Rank | null>(null);
-    const cardModel = computed((): CardModel | null => (
-      ((cardSuit.value !== null) && (cardRank.value !== null))
-        ? new CardModel(cardSuit.value, cardRank.value)
+    const _frontCardSuit = ref<Suit | null>(null);
+    const _frontCardRank = ref<Rank | null>(null);
+    const frontCardModel = computed((): CardModel | null => (
+      ((_frontCardSuit.value !== null) && (_frontCardRank.value !== null))
+        ? new CardModel(_frontCardSuit.value, _frontCardRank.value)
         : null
     ));
+    const _backCardSuit = ref<Suit | null>(null);
+    const _backCardRank = ref<Rank | null>(null);
+    const backCardModel = computed((): CardModel | null => (
+      ((_backCardSuit.value !== null) && (_backCardRank.value !== null))
+        ? new CardModel(_backCardSuit.value, _backCardRank.value)
+        : null
+    ));
+    const cardFlippedToFront = ref(frontCardModel.value !== null);
+    function _flip() {
+      cardFlippedToFront.value = !cardFlippedToFront.value;
+    }
+    function _setNull(cardSuitRef: Ref<Suit | null>, cardRankRef: Ref<Rank | null>) {
+      cardSuitRef.value = null;
+      cardRankRef.value = null;
+    }
+    function _flipAndSetNull(cardSuitRef: Ref<Suit | null>, cardRankRef: Ref<Rank | null>) {
+      _flip();
+      _setNull(cardSuitRef, cardRankRef);
+    }
     return {
-      cardSuit,
-      cardRank,
-      cardModel,
-      cardFlipped: computed(() => {
-        return (cardModel.value !== null);
-      }),
+      frontCardModel,
+      backCardModel,
+      cardFlippedToFront,
       flipBack() {
-        if (cardModel.value !== null) {
-          cardSuit.value = null;
-          cardRank.value = null;
+        if (cardFlippedToFront.value) {
+          if (frontCardModel.value !== null) {
+            _flip();
+          }
+          _setNull(_backCardSuit, _backCardRank);
+        } else {
+          if (backCardModel.value !== null) {
+            _flip();
+          }
+          _setNull(_frontCardSuit, _frontCardRank);
+        }
+      },
+      setRank(rank: Rank) {
+        if (cardFlippedToFront.value) {
+          _backCardRank.value = rank;
+          if (_frontCardSuit.value !== null) {
+            _backCardSuit.value = _frontCardSuit.value;
+          }
+          if (_backCardSuit.value !== null) {
+            _flipAndSetNull(_frontCardSuit, _frontCardRank);
+          }
+        } else {
+          _frontCardRank.value = rank;
+          if (_backCardSuit.value !== null) {
+            _frontCardSuit.value = _backCardSuit.value;
+          }
+          if (_frontCardSuit.value !== null) {
+            _flipAndSetNull(_backCardSuit, _backCardRank);
+          }
+        }
+      },
+      setSuit(suit: Suit) {
+        if (cardFlippedToFront.value) {
+          _backCardSuit.value = suit;
+          if (_frontCardRank.value !== null) {
+            _backCardRank.value = _frontCardRank.value;
+          }
+          if (_backCardRank.value !== null) {
+            _flipAndSetNull(_frontCardSuit, _frontCardRank);
+          }
+        } else {
+          _frontCardSuit.value = suit;
+          if (_backCardRank.value !== null) {
+            _frontCardRank.value = _backCardRank.value;
+          }
+          if (_frontCardRank.value !== null) {
+            _flipAndSetNull(_backCardSuit, _backCardRank);
+          }
         }
       },
       suits,
