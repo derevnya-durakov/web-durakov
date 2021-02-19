8d35a280b903d111342399ad083825f2ccb4ac49
Sergey Sokolov <mohterbaord@gmail.com>
2021-02-19 20:24:30 +0300

    поясняю за магию

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -60,7 +60,12 @@ export default defineComponent({
   position: relative;
 }
 .hoverable:hover {
-  border-radius: 6px; // NOTE: works well only with DEFAULT_CARD_SCALE!!!
+  border-radius: 6px; // NOTE: works well only with DEFAULT_CARD_SCALE = 25!!!
+                      // Explanation of Magic: SVG card images have its own border radius (12px). and their default
+                      // sizes are 250px at width and 350px at height. applying high maths we get card ratio 5/7.
+                      // then, applying DEFAULT_CARD_SCALE=25 to this ratio, we get card sizes 125px/175px what is
+                      // twice less than the original sizes. thus, the border radius should be twice less also.
+                      // so it is 6px.
   box-shadow: 0 0 10px greenyellow;
 }
 </style>
